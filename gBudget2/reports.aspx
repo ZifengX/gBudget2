d6b69ca244786785bc71684b62f4a3fc3043b65f
Zifeng Xu <xuzifeng89@gmail.com>
2015-07-28 19:49:41 -0400

    assign2Redo - simple reports

---
+++ gBudget2/reports.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="gBudget Reports" Language="C#" MasterPageFile="~/main.Master" AutoEventWireup="true" CodeBehind="reports.aspx.cs" Inherits="gBudget2.reports" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Reports</h1>
+
+    <fieldset>
+        <label for="ddlCategoryReports">Report by Category:</label>
+        <asp:DropDownList ID="ddlCategoryReport" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCategoryReport_SelectedIndexChanged" DataValueField="CategoryID" DataTextField="Category1" />
+        <asp:GridView ID="grdCategoryReport" runat="server" CssClass="table table-striped table-hover"
+             AutoGenerateColumns="false" AllowPaging="true" PageSize="3" OnPageIndexChanging="grdCategoryReport_PageIndexChanging">
+            <Columns>
+                <asp:BoundField DataField="Date" HeaderText="Date" DataFormatString="{0:yyyy-MM-dd}" />
+                <asp:BoundField DataField="Amount" HeaderText="Amount" />
+                <asp:BoundField DataField="Account1" HeaderText="Account" />
+                <asp:BoundField DataField="Mechant1" HeaderText="Mechant" />
+                <asp:BoundField DataField="Note" HeaderText="Note" />
+            </Columns>
+        </asp:GridView>
+    </fieldset>
+
+    <%--<asp:GridView ID="grdAccounts" runat="server" CssClass="table table-striped table-hover"
+        AutoGenerateColumns="false" OnRowDeleting="grdAccounts_RowDeleting" DataKeyNames="AccountID"
+        AllowPaging="true" PageSize="3" OnPageIndexChanging="grdAccounts_PageIndexChanging"
+        AllowSorting="true" OnSorting="grdAccounts_Sorting" OnRowDataBound="grdAccounts_RowDataBound" >
+        <Columns>
+            <asp:BoundField DataField="AccountID" HeaderText="Account ID" SortExpression="AccountID"/>
+            <asp:BoundField DataField="Account1" HeaderText="Account Type" SortExpression="Account1" />
+         
+            <asp:HyperLinkField HeaderText="Edit" Text="Edit" NavigateUrl="account.aspx"
+                DataNavigateUrlFields="AccountID" 
+                DataNavigateUrlFormatString="account.aspx?AccountID={0}" />
+            <asp:CommandField HeaderText="Delete" DeleteText="Delete" ShowDeleteButton="true" />
+        </Columns>
+    </asp:GridView>--%>
+</asp:Content>
---
+++ gBudget2/reports.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//model references for EF
+using gBudget2.Models;
+using System.Web.ModelBinding;
+
+
+namespace gBudget2
+{
+    public partial class reports : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if loading the page for the first time, populate report grid
+            if (!IsPostBack)
+            {
+                GetReports();
+            }
+        }
+
+        protected void GetReports()
+        {
+            using (gBudget2Entities db = new gBudget2Entities())
+            {
+                //query the datainfo table using EF and LINQ
+                var Categories = from c in db.Categories
+                                 select c;
+
+                //bind the result to the gridview
+                ddlCategoryReport.DataSource = Categories.ToList();
+                ddlCategoryReport.DataBind();
+
+                int selectedCategory = Convert.ToInt32(ddlCategoryReport.SelectedValue);
+
+                //query the bills table using EF and LINQ
+                var Datainfoes = from d in db.DataInfoes
+                                 where d.CategoryID == selectedCategory
+                                 select new { d.Date, d.Amount, d.Account.Account1, d.Mechant.Mechant1, d.Note };
+
+                grdCategoryReport.DataSource = Datainfoes.ToList();
+                grdCategoryReport.DataBind();
+
+
+                //var Accounts = from a in db.Accounts
+                //               select a;
+
+                //ddlAccount.DataSource = Accounts.ToList();
+                //ddlAccount.DataBind();
+
+                //var Mechants = from m in db.Mechants
+                //               select m;
+                //ddlMechant.DataSource = Mechants.ToList();
+                //ddlMechant.DataBind();
+            }
+        }
+
+        protected void grdCategoryReport_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //set the new page #
+            grdCategoryReport.PageIndex = e.NewPageIndex;
+            GetReports();
+        }
+
+        protected void ddlCategoryReport_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCategoryReport.SelectedValue = ddlCategoryReport.SelectedValue;
+            GetReports();
+        }
+    }
+}
